8a9c20fe7625eb9508ca158c49ee281d969f23e1
Janaka Perera <j.perera@optergy.com>
2022-04-21 14:05:48 +1000

    added chart

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,8 +2,8 @@ import "./featured.scss";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 
 const Featured = () => {
   return (
@@ -19,7 +19,8 @@ const Featured = () => {
         <p className="title">Total Sales made today</p>
         <p className="amount">$200</p>
         <p className="desc">
-          Previous transactions are processing. Last payment may not be included.
+          Previous transactions are processing. Last payment may not be
+          included.
         </p>
 
         <div className="summary">
@@ -27,21 +28,21 @@ const Featured = () => {
             <div className="itemTitle">Target</div>
             <div className="itemResult positive">
               <div className="resultAmount">$15.9k</div>
-              <ArrowDropUpIcon fontSize="small"/>
+              <ArrowDropUpIcon fontSize="small" />
             </div>
           </div>
           <div className="item">
             <div className="itemTitle">Last Week</div>
             <div className="itemResult negative">
               <div className="resultAmount">$15.9k</div>
-              <ArrowDropDownIcon fontSize="small"/>
+              <ArrowDropDownIcon fontSize="small" />
             </div>
           </div>
           <div className="item">
             <div className="itemTitle">Month</div>
             <div className="itemResult positive">
               <div className="resultAmount">$15.9k</div>
-              <ArrowDropUpIcon fontSize="small"/>
+              <ArrowDropUpIcon fontSize="small" />
             </div>
           </div>
         </div>
