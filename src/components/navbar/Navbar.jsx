9f028583b24ed2a6a991b1b02ba74921e1ed4275
Janaka Perera <j.perera@optergy.com>
2022-04-21 10:08:53 +1000

    added navbar html and css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,49 @@
-import './navbar.scss'
+import "./navbar.scss";
+import SearchIcon from "@mui/icons-material/Search";
+import LanguageIcon from "@mui/icons-material/Language";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import FullscreenExitIcon from "@mui/icons-material/FullscreenExit";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search" />
+          <SearchIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageIcon className="icon" />
+            English
+          </div>
+          <div className="item">
+            <DarkModeIcon className="icon" />
+          </div>
+          <div className="item">
+            <FullscreenExitIcon className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleIcon className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <FormatListBulletedIcon className="icon" />
+          </div>
+          <div className="item">
+            <img src="https://images.pexels.com/photos/1816642/pexels-photo-1816642.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="avatar" className="avatar" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
-import "./home.scss"
-import Sidebar from '../../components/sidebar/Sidebar'
+import "./home.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 const Home = () => {
   return (
-    <div className='home'>
-        <Sidebar/>
-        <div className="homeContainer">container</div>
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        home Container
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
